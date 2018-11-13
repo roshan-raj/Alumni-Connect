17ed93807e7a079db15ca9bd48a8e57c6a81fe5f
Roshan Raj <roshan139154@gmail.com>
2018-11-13 21:11:06 +0530

    Documentation

--- expressapp/app.js
+++ expressapp/app.js
@@ -16,10 +16,17 @@ app.use(cors({
 
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://root:root12345@ds237363.mlab.com:37363/alumni-connect');
+mongoose.connect('mongodb://root:root12345@ds237363.mlab.com:37363/alumni-connect', function(err){
+    if (!err) {
+        console.log("Database successfully connected");
+    }
+});
 
-// mongoose.connect('mongodb://localhost:27017/alumni-connect'); //for working with the project in offline mode
-
+// mongoose.connect('mongodb://localhost:27017/alumni-connect', function(err) {
+//     if (!err) {
+//         console.log("Database successfully connected");
+//     }
+// }); 
 
 //passport
 var passport = require('passport');

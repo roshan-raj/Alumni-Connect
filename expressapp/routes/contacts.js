9ef18bf1c775483e2b89aa152d5a4397a04065d5
nikhil-amin <aminnikhil073@gmail.com>
2018-11-20 08:42:29 +0530

    [FIX] Backend for contact page

--- expressapp/routes/contacts.js
+++ expressapp/routes/contacts.js
@@ -6,7 +6,6 @@ router.post('/contact', function (req, res, next) {
   addToDB(req, res);
 });
 
-
 async function addToDB(req, res) {
   var contact = new Contact({
     name: req.body.name,
@@ -23,12 +22,5 @@ async function addToDB(req, res) {
     return res.status(501).json(err);
   }
 }
-
-// router.get('/',function(req,res,next){
-//   Contact.find((err,docs) => {
-//     if(!err) {res.send(docs);}
-//     else {console.log('Err'+ JSON.stringify(err, undefined, 2));  }
-//   })
-// });
 
 module.exports = router;

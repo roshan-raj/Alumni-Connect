b5b24afe5b0507c265d6aebab2d5d925a4a71b58
Roshan Raj <roshan139154@gmail.com>
2018-12-01 10:05:19 +0530

    Validations in Register component

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {FormGroup,FormControl,Validators} from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { UserService } from '../user.service';
 
 @Component({
@@ -10,37 +10,40 @@ import { UserService } from '../user.service';
 })
 export class RegisterComponent implements OnInit {
 
-  registerForm:FormGroup = new FormGroup({
-    email:new FormControl(null,[Validators.email,Validators.required]),
-    username:new FormControl(null,Validators.required),
-    password:new FormControl(null,Validators.required),
-    cpass:new FormControl(null,Validators.required),
-    dob:new FormControl(null,Validators.required),
-    branch:new FormControl(null,Validators.required),
-    usn:new FormControl(null,Validators.required),
-    phone:new FormControl(null,Validators.required),
-    location:new FormControl(null,Validators.required),
-    classof:new FormControl(null,Validators.required),
+  registerForm: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.email, Validators.required]),
+    username: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required),
+    cpass: new FormControl(null, Validators.required),
+    dob: new FormControl(null, Validators.required),
+    branch: new FormControl(null, Validators.required),
+    usn: new FormControl(null, Validators.required),
+    phone: new FormControl(null, Validators.required),
+    location: new FormControl(null, Validators.required),
+    classof: new FormControl(null, Validators.required),
   })
-  constructor(private _router:Router, private _userService:UserService) { }
+  constructor(private _router: Router, private _userService: UserService) { }
 
   ngOnInit() {
   }
 
-  moveToLogin(){
+  moveToLogin() {
     this._router.navigate(['/login']);
   }
 
-  register(){
-    if(!this.registerForm.valid || (this.registerForm.controls.password.value != this.registerForm.controls.cpass.value)){
+  wrong: String;
+  
+  register() {
+    if (!this.registerForm.valid || (this.registerForm.controls.password.value != this.registerForm.controls.cpass.value)) {
+      this.wrong = "*Please enter valid details above";
       console.log('Invalid Form'); return;
     }
 
     this._userService.register(JSON.stringify(this.registerForm.value))
-    .subscribe(
-      data=> {console.log(data); this._router.navigate(['/login']);},
-      error=>console.error(error)
-    )
+      .subscribe(
+        data => { console.log(data); this._router.navigate(['/login']); },
+        error => console.error(error)
+      )
     // console.log(JSON.stringify(this.registerForm.value));
   }
 }
